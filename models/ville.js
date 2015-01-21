ac8b4a644384e17675bd341017b1ddf3b4bdf556
chapo-chapi <michel.chastagner@yahoo.fr>
2015-01-21 08:21:17 +0100

    modifié :         models/ville.js

--- models/ville.js
+++ models/ville.js
@@ -1,17 +1,18 @@
 var db = require('../configDb');
 
-		/**
-		 * Récupérer l'intégralité des Villes
-		 * @return Un tableau de Ville avec le N° et le nom
-		 */
+/*
+* Récupérer l'intégralité des Villes
+* @return Un tableau de Ville avec le N° et le nom
+*/
 module.exports.getListeVille = function (callback) {	
    // connection à la base
 	db.getConnection(function(err, connexion){
         if(!err){
-        	  // s'il n'y a pas d'erreur de connection
-        	  // execution de la requête SQL
+        	  // s'il n'y a pas d'erreur de connexion
+        	  // execution de la requête SQL        	  
             connexion.query('SELECT vil_num, vil_nom from ville', callback);
-            // la concection retourne dans le pool
+            
+            // la connexion retourne dans le pool
             connexion.release();
          }
       });   
