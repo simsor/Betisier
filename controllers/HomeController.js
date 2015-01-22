945ddf8475128eb7d51175567c876702cd58b4bc
DUT2 <michel.chastagner@yahoo.fr>
2015-01-22 08:02:23 +0100

    modifié :         controllers/HomeController.js 	modifié :         controllers/VilleController.js 	modifié :         views/listerVille.handlebars

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,7 +2,6 @@
 
   // ////////////////////////////////////////////// A C C U E I L
 module.exports.Index = function(request, response){
-		if(request.session.betisier){response.login=request.session.betisier}
     response.title = "Bienvenue sur le site du bêtisier de l'IUT.";
     response.render('home', response);
 };
--- controllers/VilleController.js
+++ controllers/VilleController.js
@@ -1,14 +1,28 @@
 var model = require('../models/ville.js');
    
    // ////////////////////////////////////////////// L I S T E R     V I L L E S 
+ 
+/*
+* Ce module permet de récupérer l'intégralité des Villes 
+* en utilisant la méthode getListVille du model ville.js
+* il passe listeVille  et nbVille à la vue listerVille.
+* response.listeVille contient par exemple :
+* [ { vil_num: 5, vil_nom: 'Tulle' },
+* { vil_num: 6, vil_nom: 'Brive' },
+* { vil_num: 17, vil_nom: 'Orléans' } ]
+
+* response.nbVille contient par exemple : 3
+* response.title est passé à main.handlebars via la vue listerVille
+* il sera inclus dans cette balise : <title> {{title}}</title>
+*/ 
      
 module.exports.ListerVille = function(request, response){
    response.title = 'Liste des villes';
-	if(request.session.betisier){response.login=request.session.betisier}
      
    model.getListeVille( function (err, result) {
         if (err) {
             // gestion de l'erreur
+            console.log(err);
             return;
         }
    response.listeVille = result; 
@@ -34,7 +48,7 @@ module.exports.InsertVille = function(request, response){
  	response.render('ajoutVille', response);
 };
 
-   // ////////////////////////////////////////////// M O D I F I E R     V I L L E S 
+   // ////////////////////////////////////////////// M O D I F I E R     V I L L E
      
 module.exports.ModifierVille = function(request, response){
    response.title = 'Modifier une ville';
