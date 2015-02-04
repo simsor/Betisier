f4ca58226f39f5b635b9648bc6207537a9ca9210
Rodnor <rodnor@live.fr>
2015-02-04 11:12:04 +0100

    listerCitation

--- controllers/CitationController.js
+++ controllers/CitationController.js
@@ -1,28 +1,34 @@
+var model = require('../models/citation.js');
 
-   
-// ////////////////////////////////////////////// L I S T E R     C I T A T I O N 
-   
+// ////////////////////////////////////////////// L I S T E R     C I T A T I O N
+
 module.exports.ListerCitation = 	function(request, response){
    response.title = 'Liste des citations';
-  
-      response.render('listerCitation', response);
-  } ;   
+   model.getListeCitations(function(err, result){
+     if (err) {
+       console.log(err);
+       return;
+     }
+     response.listeCitation = result;
+     response.nbCitations = result.length;
+     response.render('listerCitation', response);
+   });
+} ;
 
-// ////////////////////////////////////////////// A J O U T E R     C I T A T I O N 
-   
+// ////////////////////////////////////////////// A J O U T E R     C I T A T I O N
+
 module.exports.AjouterCitation = 	function(request, response){
 	   response.title = 'Ajouter des citations';
    response.render('ajouterCitation', response);
-     
-  } ;   
+
+  } ;
 
 
-// ////////////////////////////////////////////// R E C H E R C H E R     C I T A T I O N 
-   
+// ////////////////////////////////////////////// R E C H E R C H E R     C I T A T I O N
+
 module.exports.RechercherCitation = function(request, response){
    response.title = 'Rechercher des citations';
    response.render('rechercherCitation', response);
- 
-     		 
-  } ; 
 
+
+  } ;
--- models/citation.js
+++ models/citation.js
@@ -1,4 +1,17 @@
 var db = require('../configDb');
+module.exports.getListeCitations = function (callback) {
+  // connexion à la BD
+  db.getConnection (function(err, connexion) {
+    if (! err) {
 
+      // on récupère toutes les citations par prof, par date et avec leur moyenne
+      var requete = "SELECT c.cit_num, per_nom, per_prenom, cit_libelle, date_format(cit_date, '%d/%m/%Y') as cit_date, AVG(vot_valeur) as moyenne FROM citation c JOIN personne p ON c.per_num=p.per_num JOIN vote v ON v.cit_num=c.cit_num GROUP BY c.cit_num, per_nom, per_prenom, cit_libelle, cit_date";
 
+      // envoi de la requete a la BD
+      connexion.query(requete, callback);
 
+      // la connexion est renvoyée dans le pool de connexions
+      connexion.release();
+    }
+  });
+};
