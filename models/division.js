9302ca3ba71c0b368fdf1e1bc1586c480db637bd
Simon Garrelou <simon.garrelou@riseup.net>
2015-03-04 08:11:16 +0100

    Changement des images !

--- models/division.js
+++ models/division.js
@@ -1,9 +1,11 @@
 var db = require('../configDb');
 
 module.exports.getAllDivision = function (callback) {
-  db.getConnection(function(err, connexion){
-     req= "SELECT * FROM division";
-     connexion.query(req, callback);
-     connexion.release();
+    db.getConnection(function(err, connexion){
+	if (!err) {
+	    req= "SELECT * FROM division";
+	    connexion.query(req, callback);
+	    connexion.release();
+	}
   });
 };
