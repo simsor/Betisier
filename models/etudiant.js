434aede9eb919447b3ac6131184c5e977fcdb0f1
Rodnor <rodnor@live.fr>
2015-03-04 08:16:26 +0100

    vue ajouterSalarie + fin model etudiant + model salarie

--- models/etudiant.js
+++ models/etudiant.js
@@ -2,11 +2,11 @@ var db = require('../configDb');
 var module_personne = require("personne");
 
 module.exports.addEtudiant = function(data, callback) {
-  module_personne.addPersonne(data, function(err, result) {
+    module_personne.addPersonne(data, function(err, result) {
 
     var per_num = result.insertId;
     db.getConnection(function(err, connexion) {}
-      var req = "INSERT INTO etudiant (per_num, dep_num, div_num) VALUES(" + connexion.escape("prout") + ")"
+      var req = "INSERT INTO etudiant (per_num, dep_num, div_num) VALUES(" + per_num + ", " +connexion.escape(data.dep_num) + ", "+ connexion.escape(data.div_num) + ")"
     });
   });
 };
--- models/salarie.js
+++ models/salarie.js
@@ -1,2 +1,14 @@
-
 var db = require('../configDb');
+var module_personne = require("personne");
+
+module.exports.addSalarie = function(data, callback) {
+    module_personne.addPersonne(data, function(err, result) {
+
+      if(!err) {
+        var per_num = result.insertId;
+        db.getConnection(function(err, connexion) {}
+          var req = "INSERT INTO salarie (per_num, sal_telprof, fon_num) VALUES(" + per_num + ", " +connexion.escape(data.sal_telprof) + ", "+ connexion.escape(data.fon_num) + ")"
+      }
+    });
+  });
+};
