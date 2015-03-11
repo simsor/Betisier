ab68f8cdefba255312d64a4055e15d50c5aa0100
Simon Garrelou <simon.garrelou@riseup.net>
2015-03-11 15:59:00 +0100

    ajout de listerCitationsValidees

--- models/citation.js
+++ models/citation.js
@@ -1,11 +1,12 @@
 var db = require('../configDb');
+
 module.exports.getListeCitations = function (callback) {
   // connexion à la BD
   db.getConnection (function(err, connexion) {
     if (! err) {
 
       // on récupère toutes les citations par prof, par date et avec leur moyenne
-      var requete = "SELECT c.cit_num, per_nom, per_prenom, cit_libelle, date_format(cit_date, '%d/%m/%Y') as cit_date, AVG(vot_valeur) as moyenne FROM citation c JOIN personne p ON c.per_num=p.per_num JOIN vote v ON v.cit_num=c.cit_num GROUP BY c.cit_num, per_nom, per_prenom, cit_libelle, cit_date";
+      var requete = "SELECT c.cit_num, per_nom, per_prenom, cit_libelle, date_format(cit_date, '%d/%m/%Y') as cit_date, AVG(vot_valeur) as moyenne FROM citation c JOIN personne p ON c.per_num=p.per_num LEFT OUTER JOIN vote v ON v.cit_num=c.cit_num GROUP BY c.cit_num, per_nom, per_prenom, cit_libelle, cit_date";
 
       // envoi de la requete a la BD
       connexion.query(requete, callback);
@@ -14,4 +15,16 @@ module.exports.getListeCitations = function (callback) {
       connexion.release();
     }
   });
+};
+
+module.exports.getListeCitationsValidees = function(callback) {
+    db.getConnection(function(err, connexion) {
+	if (!err) {
+	    var requete = "SELECT c.cit_num, per_nom, per_prenom, cit_libelle, date_format(cit_date, '%d/%m/%Y') as cit_date, AVG(vot_valeur) as moyenne FROM citation c JOIN personne p ON c.per_num=p.per_num LEFT OUTER JOIN vote v ON v.cit_num=c.cit_num WHERE cit_valide=1 GROUP BY c.cit_num, per_nom, per_prenom, cit_libelle, cit_date";
+
+	    connexion.query(requete, callback);
+
+	    connexion.release();
+	}
+    });
 };
